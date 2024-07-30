bc945641a589f5e82b8f73f1bfa12fd24f8d4c1a
camscoderepo <camroman95@gmail.com>
2024-07-30 16:47:08 -0600

    worked on hero, had to add inline style because I couldn't get skeleton class to work

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -5,12 +5,20 @@
     export let buttonUrl = "#";
 </script>
 
+
+
+
 <div class="flex flex-col items-center justify-center min-h-screen p-16 text-center bg-gradient-to-r from-purple-600 to-yellow-400 text-white">
     <h1 class="text-4xl mb-2 md:text-3xl sm:text-2xl">{title}</h1>
     <p class="text-xl mb-4 md:text-lg sm:text-base">{subtitle}</p>
-    <a class="bg-yellow-400 text-white rounded-lg px-6 py-3 text-lg hover:bg-orange-400 transition duration-300" href="{buttonUrl}">
+    <a class="btn btn-white" href="{buttonUrl}">
         {buttonText}
     </a>
 </div>
   
-  
+<style>
+  .btn-white {
+    background-color: white; /* Your desired color */
+    color: black; /* Text color */
+}
+</style>
