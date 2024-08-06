8b2cf7bcb6839e1b26b0a438de655a133ce2d94c
camscoderepo <camroman95@gmail.com>
2024-08-06 15:53:26 -0600

    added image and added consulting component for now

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -21,17 +21,26 @@
 
 
 <nav class="bg-gray-800 p-4">
-    <div class="container mx-auto flex justify-between items-center">
+  <div class="container mx-auto flex justify-between items-center">
+    <div class="flex items-center space-x-24"> <!-- Adjust space-x-8 as needed -->
       <div class="text-white">Your Logo</div>
-      <Button on:click={toggleModal}  buttonText={contactBtnText}  class="text-white bg-blue-500 px-4 py-2 rounded">
-      </Button>
-      <Modal isOpen={showModal} onClose={toggleModal}>
-      </Modal>
+      <ul class="flex space-x-4">
+        <li><a href="#hero" class="text-white hover:underline">Home</a></li>
+        <li><a href="#about" class="text-white hover:underline">About Me</a></li>
+        <li><a href="#pricing" class="text-white hover:underline">Pricing</a></li>
+      </ul>
     </div>
+    <Button on:click={toggleModal} buttonText={contactBtnText} class="text-white bg-blue-500 px-4 py-2 rounded"/>
+    <Modal isOpen={showModal} onClose={toggleModal} />
+  </div>
 </nav>
 
 
 
 <style>
-
+ nav ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
 </style>
