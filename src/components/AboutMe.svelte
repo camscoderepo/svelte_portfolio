a906ba4ab5e1921626ca0ff6744bbb2f85b16087
camscoderepo <camroman95@gmail.com>
2024-08-13 19:00:02 -0600

    Added new about me image and made it round

--- src/components/AboutMe.svelte
+++ src/components/AboutMe.svelte
@@ -1,19 +1,18 @@
 <!-- AboutMe.svelte -->
 <script>
-    import aboutImage from '../assets/AboutImage.jpg';
-  </script>
-  
-  <section id="about" class="about-me container mx-auto px-4 py-8">
-    <div class="flex flex-col md:flex-row items-center">
-      <div class="md:w-1/2 md:pr-8">
-        <h2 class="text-3xl font-bold mb-4">About Me</h2>
-        <p class="text-lg">
-          Hello! I'm Cameron Roman, a freelance developer with a passion for creating beautiful and functional websites. My journey in web development has led me to work with various technologies, and I love bringing ideas to life through code.
-        </p>
-      </div>
-      <div class="md:w-1/2 mb-6 md:mb-0">
-        <img src={aboutImage} alt="weirdo" class="rounded-lg shadow-md w-full h-1/2" />
-      </div>
-    </div>
-  </section>
-  
+  import aboutImage from '../assets/me_as_bitmoji.png';
+</script>
+
+<section id="about" class="about-me container mx-auto px-4 py-8">
+<div class="flex flex-col md:flex-row items-center">
+  <div class="md:w-1/2 md:pr-8">
+    <h2 class="text-3xl font-bold mb-4">About Me</h2>
+    <p class="text-lg">
+      Hello! I'm Cameron Roman, a freelance developer with a passion for creating beautiful and functional websites. My journey in web development has led me to work with various technologies, and I love bringing ideas to life through code.
+    </p>
+  </div>
+  <div class="md:w-1/4 mb-6 md:mb-0">
+    <img src={aboutImage} alt="weirdo" class="rounded-full shadow-md w-1/2 h-auto mx-auto" /> <!-- Adjusted image size -->
+  </div>
+</div>
+</section>
