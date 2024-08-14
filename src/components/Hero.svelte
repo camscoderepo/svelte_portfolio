8fd4123a79134b452bbc3bfb70de97866f7b5942
camscoderepo <camroman95@gmail.com>
2024-08-13 19:40:42 -0600

    made more changes, needs styling and a better theme but now starting to look like a real site

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -21,7 +21,7 @@
 </script>
 
 
-<div id="home" class="flex flex-col items-center justify-center min-h-screen p-16 text-center bg-gradient-to-r from-purple-600 to-yellow-400 text-white">
+<div id="home" class="flex flex-col items-center justify-center min-h-[90vh] p-16 text-center bg-gradient-to-r from-purple-600 to-yellow-400 text-white">
     <h1 class="sm:text-2xl">{title}</h1>
     <p class="text-xl mb-4 md:text-lg sm:text-base">{subtitle}</p>
     <Button label="Hero Button" buttonText={heroBtnText} on:click={handleHeroButtonClick}></Button>
