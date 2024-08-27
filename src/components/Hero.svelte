bf4abedfbf44803f61db7a3c57045614bc51ad54
camscoderepo <camroman95@gmail.com>
2024-08-27 16:55:37 -0600

    Fixed up About Me and added video to hero section, commented it out though for now

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,7 +1,7 @@
   <script>
 	  import Button from "./Button.svelte";
-      import { createEventDispatcher } from "svelte";
-
+    import { createEventDispatcher } from "svelte";
+    // import coding from '$lib/assets/coding.mp4'
       const dispatch = createEventDispatcher();
 
       let currentPage = 'home';
@@ -19,9 +19,19 @@
     
 </script>
 
-
-<div id="home" class="flex flex-col items-center justify-center min-h-[90vh] p-16 text-center bg-gradient-to-r from-purple-600 to-yellow-400 text-white">
-    <h1 class="sm:text-2xl">{title}</h1>
-    <p class="text-xl mb-4 md:text-lg sm:text-base">{subtitle}</p>
-    <Button label="Hero Button" buttonText={heroBtnText} on:click={handleHeroButtonClick}></Button>
-</div>
+<!-- <section class="relative overflow-hidden">
+  <video
+  autoplay
+  muted
+  loop
+  class="absolute top-0 left-0 w-full h-full object-cover"
+  >
+  <source src="{coding}" type="video/mp4" />
+  Your browser does not support the video tag.
+  </video> -->
+  <div id="home" class="flex flex-col items-center justify-center min-h-[90vh] p-16 text-center bg-gradient-to-r from-purple-600 to-yellow-400 text-white">
+      <h1 class="sm:text-2xl">{title}</h1>
+      <p class="text-xl mb-4 md:text-lg sm:text-base">{subtitle}</p>
+      <Button label="Hero Button" buttonText={heroBtnText} on:click={handleHeroButtonClick}></Button>
+  </div>
+<!-- </section> -->
