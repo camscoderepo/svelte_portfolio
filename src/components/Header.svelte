5de2ccceca0207f808eff5bc63bf8e6e4a963cbb
camscoderepo <camroman95@gmail.com>
2024-09-05 16:12:17 -0600

    Started reimplementing contact me button and have reCAPTCHA added as well

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -46,9 +46,9 @@
 <nav class="bg-gray-100 dark:bg-gray-800 p-4 z-20 fixed top-0 left-0 right-0">
   <div class="container mx-auto flex justify-between items-center">
     <div class="text-gray-800 dark:text-white">Cameron Roman</div>
-
-    <!-- <Button on:click={openModal} buttonText={modalBtnText} /> -->
     <LightSwitch />
+    <Button onClick={openModal}>Contact Me</Button>
+    
 
     <!-- Mobile Navigation Button -->
      <!-- {#if isMobile}
---
+++ src/types/recaptcha.d.ts
@@ -0,0 +1,11 @@
+// src/types/recaptcha.d.ts
+
+interface ReCaptcha {
+    enterprise: {
+      ready(callback: () => void): void;
+      execute(siteKey: string, options: { action: string }): Promise<string>;
+    };
+  }
+  
+  declare const grecaptcha: ReCaptcha;
+  
