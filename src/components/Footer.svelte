8c876ac747157c47dd82e184981b2d48e8cdc0bb
camscoderepo <camroman95@gmail.com>
2025-02-24 12:15:10 -0700

    moer changes to footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -21,7 +21,7 @@
             <FontAwesomeIcon icon={faHeart} />
         </div>
     </div>
-    <div class="flex items-center pl-[40px]">
+    <div class="flex items-center pl-[50px]">
         <a href="mailto:{email}" class="text-white mx-2">
             <FontAwesomeIcon icon={faEnvelope} />
             <span class="sr-only">Email</span> <!-- Screen reader only text -->
