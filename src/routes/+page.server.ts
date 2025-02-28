00051ad2403934a0f88ed7b33399685bc49e426c
camscoderepo <camroman95@gmail.com>
2025-02-28 15:22:23 -0700

    maybe fixed things....

--- src/routes/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import { put } from '@vercel/blob';
-
-export async function PUT(request: Request) {
-  const { url } = await put('/lib/assets/new_coding.mp4', request.body, { access: 'public' });
- 
-  return Response.json({
-    url
-  });
-}
